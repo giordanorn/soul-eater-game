8adccceeeea5b355db92e89ce79982895556278d
Giordano Rodrigues <22085029+giordanorn@users.noreply.github.com>
2019-06-04 22:12:05 -0300

    Script da barra de HP adicionado

---
+++ Assets/Scripts/HUD/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class HealthBar : MonoBehaviour
+{
+    public HealthReserve healthReserve;
+    private float barMaxWidth;
+    private RectTransform rectTransform;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        barMaxWidth = rectTransform.sizeDelta.x;
+    }
+
+    void Update()
+    {
+        float width = barMaxWidth * (healthReserve.CurrentHealth / healthReserve.maximumHealth);
+        float height = rectTransform.sizeDelta.y;
+
+        rectTransform.sizeDelta = new Vector2(width, height);
+    }
+}
