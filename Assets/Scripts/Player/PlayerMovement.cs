b40d80b1cbb705323392204c5a71c04f4f50ab30
JohnVithor <jv.venceslau.c@gmail.com>
2019-06-06 16:21:20 -0300

    Adição de alguns comentários ao código

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,18 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Transform))]
 public class PlayerMovement : MonoBehaviour
 {
+    /***** Unity Parameters *****/
+
+    /// <summary>The Key that move the GameObject upwards.</summary>
     public KeyCode up = KeyCode.W;
+    /// <summary>The Key that move the GameObject downward.</summary>
     public KeyCode down = KeyCode.S;
+    /// <summary>The Key that move the GameObject to left.</summary>
     public KeyCode left = KeyCode.A;
+    /// <summary>The Key that move the GameObject to right.</summary>
     public KeyCode right = KeyCode.D;
+    /// <summary>The speed that the GameObject moves.</summary>
     public float speed = 3f;
+
+    /***** Unity Methods *****/
     void FixedUpdate()
     {
         Move();
     }
 
+    /// <summary>Get the Keys pressed and move the GameObject</summary>
     void Move()
     {
         float Hmove = 0.0f;
