63eb97a04d9f88f9f18438ac4057f6b4e9b6bcd3
JohnVithor <jv.venceslau.c@gmail.com>
2019-06-08 13:40:16 -0300

    Script para tornar um GameObject coletável.

---
+++ Assets/Scripts/Itens/Collectible.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Collectible : MonoBehaviour
+{
+    private Collider2D coll2d;
+    public Effect[] effects;
+
+    private void Awake()
+    {
+        coll2d = GetComponent<Collider2D>();
+        effects = GetComponents<Effect>();
+        coll2d.isTrigger = true;
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        Collector collector = col.GetComponent<Collector>();
+        if (collector != null)
+        {
+            foreach (Effect effect in effects)
+            {
+                effect.Start(collector);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
