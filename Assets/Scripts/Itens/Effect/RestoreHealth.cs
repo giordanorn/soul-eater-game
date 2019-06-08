ffae63cd7d81b05f137e089b6ae3c72c248dab5c
JohnVithor <jv.venceslau.c@gmail.com>
2019-06-08 13:38:52 -0300

    Exemplo de efeito criado. Restaura a vida.

---
+++ Assets/Scripts/Itens/Effect/RestoreHealth.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class RestoreHealth : Effect
+{
+    public float value = 10;
+    public override void Start(Collector collector)
+    {
+        if (collector.IsCreature())
+        {
+            HealthReserve healthReserve = collector.GetComponent<HealthReserve>();
+            if (healthReserve != null)
+            {
+                healthReserve.Increase(value);
+            }
+            else
+            {
+                Debug.LogWarning("Cannot restore health. HealthReserve missing!");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Cannot restore health. Not a Creature!");
+        }
+    }
+}
