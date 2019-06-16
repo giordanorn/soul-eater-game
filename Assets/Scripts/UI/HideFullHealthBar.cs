f923619fa8aa0ce3bdb2a051367fb5d58429e547
JohnVithor <jv.venceslau.c@gmail.com>
2019-06-16 14:06:46 -0300

    Script para controlar a visualização da HealthBar
    Deve ser adicionado a um gameObject com a HealthBar e possuir uma imagem.
    Quando a vida está cheia ele desativa a imagem.

---
+++ Assets/Scripts/UI/HideFullHealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(HealthBar), typeof(Image))]
+public class HideFullHealthBar : MonoBehaviour
+{
+
+    private HealthBar healthBar;
+    private Image image;
+
+    void Start()
+    {
+        healthBar = GetComponent<HealthBar>();
+        image = GetComponent<Image>();
+
+    }
+    void Update()
+    {
+        if (healthBar.healthReserve.CurrentHealth == healthBar.healthReserve.maximumHealth)
+        {
+            image.enabled = false;
+        }
+        else
+        {
+            image.enabled = true;
+        }
+    }
+}
