d325d6adb2cc0076b440c2d066aa669fa4b48747
Giordano Rodrigues <22085029+giordanorn@users.noreply.github.com>
2019-06-17 01:01:13 -0300

    Ataque curto devidamente implementado
    Alguns scripts foram quebrados em mais de um

--- Assets/Scripts/Creatures/PlayerShortAttack.cs
+++ Assets/Scripts/Creatures/PlayerShortAttack.cs
@@ -7,20 +7,15 @@ public class PlayerShortAttack : MonoBehaviour
 {
     public float range = 1;
     public float attackDuration = 0.1f;
-    public float attackCooldown = 0.5f;
-    public KeyCode hitKey = KeyCode.Space;
-
+    //public float attackCooldown = 0.5f;
+    
     private CircleCollider2D attackCollider;
 
     private Attacker attacker;
-
-    private Vector2 downOffset;
-    private Vector2 leftOffset;
-    private Vector2 upOffset;
-    private Vector2 rightOffset;
+    private PlayerDirection playerDirection;
 
     private Cooldown duration;
-    private Cooldown cooldown;
+    //private Cooldown  cooldown;
 
     void Start()
     {
@@ -29,54 +24,12 @@ public class PlayerShortAttack : MonoBehaviour
         
         attacker = GetComponentInParent<Attacker>();
 
-        downOffset = new Vector2(0, -range);
-        leftOffset = new Vector2(-range, 0);
-        upOffset = new Vector2(0, range);
-        rightOffset = new Vector2(range, 0);
-
         duration = gameObject.AddComponent<Cooldown>();
-        cooldown = gameObject.AddComponent<Cooldown>();
-    }
+        duration.onCooldownEnd.AddListener(StopAttack);
 
-    void Update()
-    {
-        UpdateAttackCollider();
-        if (Input.GetKeyDown(hitKey) && !cooldown.InCooldown())
-        {
-            PerformShortAttack();
-            cooldown.StartCooldown(attackCooldown);
-        }
-        if (!duration.InCooldown())
-        {
-            attackCollider.enabled = false;
-        }
-    }
-
-    private void UpdateAttackCollider()
-    {
-        switch (GetComponentInParent<SpriteDirectionController>().Direction)
-        {
-            case SpriteDirectionController.Directions.Down:
-                attackCollider.offset = downOffset;
-                break;
-            case SpriteDirectionController.Directions.Left:
-                attackCollider.offset = leftOffset;
-                break;
-            case SpriteDirectionController.Directions.Up:
-                attackCollider.offset = upOffset;
-                break;
-            case SpriteDirectionController.Directions.Right:
-                attackCollider.offset = rightOffset;
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void PerformShortAttack()
-    {
-        attackCollider.enabled = true;
-        duration.StartCooldown(attackDuration);
+        playerDirection = GetComponentInParent<PlayerDirection>();
+        playerDirection.OnDirectionChange.AddListener(PerformAttack);
+        // cooldown = gameObject.AddComponent<Cooldown>();
     }
 
     void OnTriggerEnter2D(Collider2D collider)
@@ -86,5 +39,32 @@ public class PlayerShortAttack : MonoBehaviour
         {
             attacker.Hit(target);
         }
+    }
+
+    private void PerformAttack(Direction direction)
+    {
+        attackCollider.offset = GetOffset(direction);
+        attackCollider.enabled = true;
+        duration.StartCooldown(attackDuration);
+    }
+
+    private Vector2 GetOffset(Direction direction)
+    {
+        if (direction == Direction.Down)
+            return new Vector2(0, -range);
+        else if (direction == Direction.Left)
+            return new Vector2(-range, 0);
+        else if (direction == Direction.Up)
+            return new Vector2(0, range);
+        else if (direction == Direction.Right)
+            return new Vector2(range, 0);
+        else
+            return Vector2Int.zero;
+    }
+
+    public void StopAttack()
+    {
+        attackCollider.enabled = false;
+        attackCollider.offset = Vector2Int.zero;
     }
 }
