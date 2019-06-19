c5c2bdd0f9cb54e1dd667ef8867e8481df329819
João Pedro Holanda <jpholanda.prf@gmail.com>
2019-06-19 11:03:11 -0300

    Refatoração e adaptação do Tiling para chunks
    Tiling tem uma única responsabilidade: renderizar os tiles. Foi adaptado agora para utilizar o controlador de chunks para adicionar/remover os tiles do Tilemap.

--- Assets/Scripts/Map/Tiling.cs
+++ Assets/Scripts/Map/Tiling.cs
@@ -1,83 +1,107 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 [RequireComponent(typeof(Tilemap))]
+[RequireComponent(typeof(ChunkController))]
+[RequireComponent(typeof(MapModel))]
 public class Tiling : MonoBehaviour
 {
-    /***** Unity Parameters *****/
+    /***** Unity Params *****/
 
-    /// <summary>The size of the grid along the x axis.</summary>
-    public int xSize = 100;
-
-    /// <summary>The size of the grid along the y axis.</summary>
-    public int ySize = 100;
-
-    /// <summary>The tiles to be used.</summary>
-    public Tile[] tiles;
+    /// <summary>The tile to be used.</summary>
+    public Tile tile;
 
 
-    /***** Internal *****/
+    /***** Unity Methods *****/
 
-    /// <summary>The tilemap component</summary>
-    private Tilemap tilemap;
-
-    /// <summary>The partitioning component.</summary>
-    private Partitioning partitioning;
-
-
-    /***** API *****/
-
-    /// <summary>Tile this instance.</summary>
-    [ContextMenu("Generate Tiles")]
-    public void Tile()
+    void Awake()
     {
-        // If there are no tiles, there is nothing to do.
-        if (tiles == null || tiles.Length == 0)
-            return;
-
         tilemap = GetComponent<Tilemap>();
-        partitioning = GetComponent<Partitioning>();
+        chunkController = GetComponent<ChunkController>();
+        mapModel = GetComponent<MapModel>();
+    }
 
-        // Associate each region with a color.
-        IDictionary<Vector3Int, Color> regionColors = new Dictionary<Vector3Int, Color>();
+    IEnumerator Start()
+    {
+        // Wait for Chunk Controller to be ready.
+        yield return new WaitUntil(() => chunkController.isActiveAndEnabled);
 
-        // Iterate over all possible grid positions.
-        for (int x = 0; x < xSize; x++)
+        Tile(chunkController.ActiveChunks, new RectInt[] { });
+
+        chunkController.onSwappingChunks.AddListener(Tile);
+    }
+
+    /// <summary>Generates tiles for in-editor tile visualization.</summary>
+    [ContextMenu("Generate Tiles")]
+    void GenerateTiles()
+    {
+        tilemap = GetComponent<Tilemap>();
+        mapModel = GetComponent<MapModel>();
+        regionColors.Clear();
+
+        for (int x = 0; x < mapModel.mapSize; x++)
         {
-            for (int y = 0; y < ySize; y++)
+            for (int y = 0; y < mapModel.mapSize; y++)
             {
-                // The current position in the grid.
-                Vector3Int tilePos = new Vector3Int(x, y, 0);
-
-                // Get a random tile from the tile palette.
-                int tileIndex = Random.Range(0, tiles.Length);
-
-                // Set the tile in the current grid position. 
-                tilemap.SetTile(tilePos, tiles[tileIndex]);
-
-                // Check if tilemap has partitions.
-                if (partitioning != null && partitioning.RegionCenters.Count > 0)
+                Vector2Int pos = new Vector2Int(x, y);
+                Vector2Int center = Vector2Int.FloorToInt(mapModel.CenterOf(pos));
+                if (!regionColors.TryGetValue(center, out Color color))
                 {
-                    // Remove color lock for this tile.
-                    tilemap.SetTileFlags(tilePos, TileFlags.InstantiateGameObjectRuntimeOnly | TileFlags.LockTransform);
-
-                    // Figure out which region this tile belongs to.
-                    Vector3Int center = Vector3Int.RoundToInt(partitioning.CenterOf(tilePos));
-
-                    // If this region has no color associated, assign a random one to it.
-                    if (!regionColors.TryGetValue(center, out Color color))
-                    {
-                        color = Random.ColorHSV();
-                        regionColors.Add(center, color);
-                    }
-
-                    // Set the color for this tile.
-                    tilemap.SetColor(tilePos, color);
+                    color = Random.ColorHSV();
+                    regionColors.Add(center, color);
                 }
+
+                tilemap.SetTile((Vector3Int)pos, tile);
+                tilemap.SetTileFlags((Vector3Int)pos, TileFlags.InstantiateGameObjectRuntimeOnly | TileFlags.LockTransform);
+                tilemap.SetColor((Vector3Int)pos, color);
             }
         }
     }
 
+    /***** Internal *****/
 
+    /// <summary>The tilemap.</summary>
+    private Tilemap tilemap;
+
+    /// <summary>The chunk controller.</summary>
+    private ChunkController chunkController;
+
+    /// <summary>The map model.</summary>
+    private MapModel mapModel;
+
+    /// <summary>Associates each region with a color.</summary>
+    private readonly Dictionary<Vector2Int, Color> regionColors = new Dictionary<Vector2Int, Color>();
+
+    /// <summary>Tile the map according to the chunks <paramref name="added"/> and <paramref name="removed"/>.</summary>
+    /// <param name="added">Added.</param>
+    /// <param name="removed">Removed.</param>
+    private void Tile(IEnumerable<RectInt> added, IEnumerable<RectInt> removed)
+    {
+        foreach (RectInt chunk in added)
+        {
+            foreach (Vector2Int pos in chunk.allPositionsWithin)
+            {
+                Vector2Int center = Vector2Int.FloorToInt(mapModel.CenterOf(pos));
+                if (!regionColors.TryGetValue(center, out Color color))
+                {
+                    color = Random.ColorHSV();
+                    regionColors.Add(center, color);
+                }
+
+                tilemap.SetTile((Vector3Int)pos, tile);
+                tilemap.SetTileFlags((Vector3Int)pos, TileFlags.InstantiateGameObjectRuntimeOnly | TileFlags.LockTransform);
+                tilemap.SetColor((Vector3Int)pos, color);
+            }
+        }
+
+        foreach (RectInt chunk in removed)
+        {
+            foreach (Vector2Int pos in chunk.allPositionsWithin)
+            {
+                tilemap.SetTile((Vector3Int)pos, null);
+            }
+        }
+    }
 }
