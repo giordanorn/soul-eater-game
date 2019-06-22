556c30dd177b3800daecbeebad123fecc09aae08
JohnVithor <jv.venceslau.c@gmail.com>
2019-06-22 20:55:17 -0300

    Script para mostrar o tempo vivo no jogo

---
+++ Assets/Scripts/UI/TimeOnGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeOnGame : MonoBehaviour
+{
+    public PlayerStatistics playerStatistics;
+    public Text timeSurvived;
+
+    void Update()
+    {
+        timeSurvived.text = playerStatistics.timeAlive.ToString("F2");
+    }
+}
